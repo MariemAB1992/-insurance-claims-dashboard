f46cd35a52aa0dfb17344a03109f558bc5a58fdf
MariemAB1992 <52740149+MariemAB1992@users.noreply.github.com>
2019-07-10 12:22:57 +0100

    Update ui.R

--- ui.R
+++ ui.R
@@ -8,40 +8,6 @@ library(ECharts2Shiny)
 library(RODBC)
 library(DBI)
 library(Rcpp)
-odbc.key = odbcConnect("MAGBI", uid="bi", pwd="pwdbi")
-ex <- sqlQuery( channel= odbc.key, query = "SELECT POL,agent ,TARIF as usage ,CLBM as bonmal,DTEMIS as date,PTOT,CAST( 
-                CASE 
-                WHEN (INC != '0' or VOL != '0') and DCL ='0' and PRDOM='0' 
-                THEN 'VOLINC'
-                
-                WHEN RCRTI!='0' and VOL='0' and INC='0' and DCL='0' and PRDOM='0'
-                THEN 'RC'
-                
-                WHEN DCL!='0'
-                THEN 'DC'
-                WHEN PRDOM!='0'
-                THEN 'TR'  
-                
-                END AS char) as garantie
-                FROM [MAGBI].[dbo].[biemis] where TYPQ='2' and RISQ='30' and PTOT>5.7 ")
-
-ex1<- sqlQuery( channel= odbc.key, query ="SELECT nant, pol
-                FROM [MAGBI].[dbo].[Binant] ")
-ex2<-sqlQuery( channel= odbc.key, query ="SELECT pol,mec
-               FROM [MAGBI].[dbo].[BIvehicule] ")
-
-ex3<-merge(ex,ex1,by.x='POL',by.y='pol',all.x=TRUE)
-ex3[is.na(ex3)]<-'x'
-ex4<-merge(ex3,ex2,by.x='POL',by.y='pol',all.x=TRUE)
-vie<-difftime(ex4$date, ex4$mec, units = "days")
-ex4<-cbind(ex4,vie)
-ex4<-ex4[,c(2,3,4,5,6,7,8,9,10,1)]
-ex4<-ex4[,-8]
-
-DATA<-as.data.frame(ex4)
-
-
-
 ui <- dashboardPage(skin ="yellow",
                     dashboardHeader(title = " Production", tags$head(tags$link(rel = "icon", type = "image/png", href = "favicon.png"),
                                                                      tags$title("Production"))  ),
